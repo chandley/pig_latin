0a9f8e2054f71437eff07a7e3a482c2eceb8406e
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-22 07:43:14 +0100

    basic function - 4 examples all work

--- piglatin.rb
+++ piglatin.rb
@@ -2,16 +2,30 @@
 def latin(english)
 
 	if m = /^[aeiou]/.match(english)
-		puts "began with vowel #{m[0]}"
+#		puts "began with vowel #{m[0]}"
+#		puts "rest of string #{english[m.end(0)..-1]}"
+		return english+"-way"
 	elsif m = /^[b-df-hj-np-tv-z]+/.match(english)
-		puts "began with consonants #{m[0]}"
+#		puts "began with consonants #{m[0]}"
+#		puts "rest of string #{english[m.end(0)..-1]}"
+		return english[m.end(0)..-1] + "-" +  m[0] + "ay"
+	else
+		puts "no match - perhaps began with space or non-alphanumeric"
 	end
 
 	
 end
 
-latin "allow"
-latin "follow"
-latin "cat"
-latin "zebra"
+puts latin "art"
+puts latin "eagle"
+
+puts latin "start"
+puts latin "door"
+
+#latin "allow"
+#latin "follow"
+#latin "cat"
+#latin "zebra"
 latin "knight"
+latin " knight"
+latin "**hello"
