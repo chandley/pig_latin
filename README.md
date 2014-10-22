3a80d060b060f363de04906375eac5c3e5c9213b
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-22 09:21:13 +0100

    update comments, README

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
+Converts from English to Pig-Latin, and back again
+
 Pig Latin is an English-language word game, usually played by children, in which words are mutated systematically. 
+
 A word that begins with a vowel (a, e, i, o, or u) has the syllable “way” added to the end; for instance, art becomes art-way and eagle becomes eagle-way. 
+
 A word that begins with one or more consonants has the initial consonants stripped, moved to the end of the word, then the syllable “ay” is added; for instance, start becomes art-stay and door becomes oor-day. 
-A hyphen is added as shown above as an aid to prevent ambiguity; otherwise, a word like aspray could be the translation of spray (ay-spray) or prays (ays-pray). 
+
+A hyphen is added as shown above as an aid to prevent ambiguity; otherwise, a word like aspray could be the translation of spray (ay-spray) or prays (ays-pray).
+
+Functions have some issues with the handling of capitalisation, and treatment of words which begin with 'w' followed by a vowel (tranlsation back into english is ambiguous) 
--- piglatin.rb
+++ piglatin.rb
@@ -1,6 +1,6 @@
+# has problems with words beginning w followed by vowel - translation back to english is ambigous
 
 def to_latin(english)
-
 	if m = /^[aeiouAEIOU]/.match(english)	# begins with vowel, add -way to end
 		return english+"-way"
 	elsif m = /^[b-df-hj-np-tv-z]+/.match(english.downcase) # begin with consonants
@@ -11,16 +11,18 @@ def to_latin(english)
 end
 
 def to_english(latin)
-	if m = /-way$/.match(latin) # english vowel word ends -way
-		return latin[0..m.begin(0)-1]	#ends in way - remove way
-	elsif m = /-[b-df-hj-np-tv-z]+ay/.match(latin) # english consonant word ends -***ay
-		return m[0][1..-3] + latin[0..m.begin(0)-1] # recover consonants from ending, follow rest
+	if m = /-way$/.match(latin) # english vowel word ends in -way
+		return latin[0..m.begin(0)-1]	# remove -way
+	elsif m = /-[b-df-hj-np-tv-z]+ay$/.match(latin) # english consonant word ends -***ay
+		return m[0][1..-3] + latin.downcase[0..m.begin(0)-1] # recover consonants from ending, follow with rest
 	else
 		puts "no match - perhaps began with space or non-alphanumeric"
 	end	
 end
 
-%w{The quick brown fox jumped over the lazy dog}.each {|word| print to_latin(word) + " "}
+# test the two functions
+text_array = %w{The quick brown fox jumped over the lazy dog}
+text_array.each {|word| print to_latin(word) + " "}
 puts
-
-%w{e-thay uick-qay own-bray ox-fay umped-jay over-way e-thay azy-lay og-day }.each {|word| print to_english(word) +  " "}
+text_array = %w{e-thay uick-qay own-bray ox-fay umped-jay over-way e-thay azy-lay og-day}
+text_array.each {|word| print to_english(word) +  " "}
