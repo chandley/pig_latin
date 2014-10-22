88be74b49205c1bc65481f1a9eb0bed0dc52df0a
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-22 09:01:22 +0100

    add latin to english function

--- piglatin.rb
+++ piglatin.rb
@@ -1,31 +1,26 @@
 
-def latin(english)
+def to_latin(english)
 
-	if m = /^[aeiou]/.match(english)
-#		puts "began with vowel #{m[0]}"
-#		puts "rest of string #{english[m.end(0)..-1]}"
+	if m = /^[aeiouAEIOU]/.match(english)	# begins with vowel, add -way to end
 		return english+"-way"
-	elsif m = /^[b-df-hj-np-tv-z]+/.match(english)
-#		puts "began with consonants #{m[0]}"
-#		puts "rest of string #{english[m.end(0)..-1]}"
-		return english[m.end(0)..-1] + "-" +  m[0] + "ay"
+	elsif m = /^[b-df-hj-np-tv-z]+/.match(english.downcase) # begin with consonants
+		return english[m.end(0)..-1] + "-" +  m[0] + "ay" # move consonants to end, add ay
 	else
 		puts "no match - perhaps began with space or non-alphanumeric"
 	end
-
-	
 end
 
-puts latin "art"
-puts latin "eagle"
+def to_english(latin)
+	if m = /-way$/.match(latin) # english vowel word ends -way
+		return latin[0..m.begin(0)-1]	#ends in way - remove way
+	elsif m = /-[b-df-hj-np-tv-z]+ay/.match(latin) # english consonant word ends -***ay
+		return m[0][1..-3] + latin[0..m.begin(0)-1] # recover consonants from ending, follow rest
+	else
+		puts "no match - perhaps began with space or non-alphanumeric"
+	end	
+end
 
-puts latin "start"
-puts latin "door"
+%w{The quick brown fox jumped over the lazy dog}.each {|word| print to_latin(word) + " "}
+puts
 
-#latin "allow"
-#latin "follow"
-#latin "cat"
-#latin "zebra"
-latin "knight"
-latin " knight"
-latin "**hello"
+%w{e-thay uick-qay own-bray ox-fay umped-jay over-way e-thay azy-lay og-day }.each {|word| print to_english(word) +  " "}
